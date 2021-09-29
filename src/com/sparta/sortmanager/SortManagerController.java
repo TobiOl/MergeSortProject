4ccd0a39f64245487da477aaef61296a1b1b1c3d
TobiOl <1712264@brunel.ac.uk>
2021-09-29 08:26:43 +0100

    more progress on MVC refactor, moving to MVC branch

--- src/com/sparta/sortmanager/SortManagerController.java
+++ src/com/sparta/sortmanager/SortManagerController.java
@@ -1,7 +1,46 @@
 package com.sparta.sortmanager;
 
+import java.util.Scanner;
+
 public class SortManagerController {
-    private int choice;
+    private String choice1;
+    private int choice2;
     private int[] unsortedArray;
 
+    public SortManagerController(String choice1, int choice2, int[] unsortedArray) {
+        this.choice1 = choice1;
+        this.choice2 = choice2;
+        this.unsortedArray = unsortedArray;
+    }
+
+    public String getChoice1() {
+        return choice1;
+    }
+
+    public void setChoice1(String choice1) {
+        this.choice1 = choice1;
+    }
+    public int getChoice2() {
+        return choice2;
+    }
+
+    public void setChoice2(int choice2) {
+        this.choice2 = choice2;
+    }
+
+    public int[] getUnsortedArray() {
+        return unsortedArray;
+    }
+
+    public void setUnsortedArray(int[] unsortedArray) {
+        this.unsortedArray = unsortedArray;
+    }
+
+    public void chooseSort(){
+        Scanner val1 = new Scanner((System.in));
+        Scanner val2 = new Scanner((System.in));
+        setChoice1(val1.next());
+        setChoice2(val2.nextInt());
+        setUnsortedArray(GenerateArray.generateIntArray(choice2));
+    }
 }
